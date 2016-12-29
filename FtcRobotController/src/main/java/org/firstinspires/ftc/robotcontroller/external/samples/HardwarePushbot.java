5f58cfe41355c044cdccc06409abe40fd1bb7f05
DESKTOP-OJSODES\Owner <aman.shaik.us@gmail.com>
2016-12-29 11:56:19 -0500

    first upload of SparkyTank and HW map

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwarePushbot.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwarePushbot.java
@@ -7,39 +7,43 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * This is NOT an opmode.
- *
+ * <p>
  * This class can be used to define all the specific hardware for a single robot.
  * In this case that robot is a Pushbot.
  * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
- *
+ * <p>
  * This hardware class assumes the following device names have been configured on the robot:
  * Note:  All names are lower case and some have single spaces between words.
- *
+ * <p>
  * Motor channel:  Left  drive motor:        "left_drive"
  * Motor channel:  Right drive motor:        "right_drive"
  * Motor channel:  Manipulator drive motor:  "left_arm"
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
-public class HardwarePushbot
-{
+public class HardwarePushbot {
     /* Public OpMode members. */
-    public DcMotor  leftMotor   = null;
-    public DcMotor  rightMotor  = null;
-    public DcMotor  armMotor    = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
+    public DcMotor leftMotor = null;
+    public DcMotor rightMotor = null;
+    public DcMotor pickMotor = null;
+    public DcMotor leftShoot = null;
+    public DcMotor rightShoot = null;
+    public Servo guider = null;
+    //public DcMotor  armMotor    = null;
+    //public Servo    leftClaw    = null;
+    //public Servo    rightClaw   = null;
 
-    public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
+    // public static final double MID_SERVO       =  0.5 ;
+    //public static final double ARM_UP_POWER    =  0.45 ;
+    //public static final double ARM_DOWN_POWER  = -0.45 ;
 
     /* local OpMode members. */
-    HardwareMap hwMap           =  null;
-    private ElapsedTime period  = new ElapsedTime();
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+    //public ServoAccess leftClaw;
 
     /* Constructor */
-    public HardwarePushbot(){
+    public HardwarePushbot() {
 
     }
 
@@ -49,41 +53,42 @@ public class HardwarePushbot
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftMotor   = hwMap.dcMotor.get("left_drive");
-        rightMotor  = hwMap.dcMotor.get("right_drive");
-        armMotor    = hwMap.dcMotor.get("left_arm");
-        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-
+        leftMotor = hwMap.dcMotor.get("left_drive");
+        rightMotor = hwMap.dcMotor.get("right_drive");
+        pickMotor = hwMap.dcMotor.get("left_pick");
+        leftShoot = hwMap.dcMotor.get("left_shoot");
+        rightShoot = hwMap.dcMotor.get("right_shoot");
+        guider=hwMap.servo.get("guide_shot");
+        //pickMotor   =hwMap.dcMotor.get("right_pick")
+        //armMotor    = hwMap.dcMotor.get("left_arm");
+        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        //rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
         // Set all motors to zero power
         leftMotor.setPower(0);
         rightMotor.setPower(0);
-        armMotor.setPower(0);
-
+        pickMotor.setPower(0);
+        leftShoot.setPower(0);
+        rightShoot.setPower(0);
+        guider.setPosition(0);
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        // Define and initialize ALL installed servos.
-        leftClaw = hwMap.servo.get("left_hand");
-        rightClaw = hwMap.servo.get("right_hand");
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
+        pickMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftShoot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightShoot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     /***
-     *
      * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
      * periodic tick.  This is used to compensate for varying processing times for each cycle.
      * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
      *
-     * @param periodMs  Length of wait cycle in mSec.
+     * @param periodMs Length of wait cycle in mSec.
      */
     public void waitForTick(long periodMs) {
 
-        long  remaining = periodMs - (long)period.milliseconds();
+        long remaining = periodMs - (long) period.milliseconds();
 
         // sleep for the remaining portion of the regular cycle period.
         if (remaining > 0) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkyPickShootTank.java
@@ -0,0 +1,172 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+/**
+ * This file provides basic Telop driving for a Pushbot robot.
+ * The code is structured as an Iterative OpMode
+ * <p>
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ * <p>
+ * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ * <p>
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "SparkyTank", group = "Pushbot")
+//@Disabled
+public class SparkyPickShootTank extends OpMode {
+
+    /* Declare OpMode members. */
+    HardwarePushbot robot = new HardwarePushbot(); // use the class created to define a Pushbot's hardware
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("", "G O  S P A R K Y !");    //
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+        boolean pickerIn;
+        boolean pickerOut;
+        double leftShoot;
+        double rightShoot;
+        boolean guider;
+        //double guiderDown
+        boolean shooter;
+        double INCREMENT = 0.1;     // amount to slew servo each CYCLE_MS cycle
+        double MAX_POS = 1.0;     // Maximum rotational position
+        double MIN_POS = 0.0;     // Minimum rotational position
+        double position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+        //boolean rampUp = true;
+        // Rheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        //Driver
+
+        //driver
+        left = -gamepad1.left_stick_y;
+        right = gamepad1.right_stick_y;
+        //picker
+        pickerOut = gamepad1.left_bumper;
+        pickerIn = gamepad1.right_bumper;
+        //shooter
+        shooter = gamepad1.y;
+        //guider
+        guider = gamepad1.b;
+        //guiderDown=gamepad1.a
+        robot.leftMotor.setPower(left);
+        robot.rightMotor.setPower(right);
+        if (pickerIn)
+            robot.pickMotor.setPower(-1);
+        if (pickerOut)
+            robot.pickMotor.setPower(1);
+        if (shooter) {
+            robot.leftShoot.setPower(1);
+            robot.rightShoot.setPower(-1);
+        } else {
+            robot.leftShoot.setPower(0);
+            robot.rightShoot.setPower(0);
+        }
+        //guider to push particle through shooter
+      /*  if (guider) {
+            robot.guider.setPosition(0.7);
+        } else {
+            robot.guider.setPosition(0.2);
+        }*/
+        if (guider) {
+            // Keep stepping up until we hit the max value.
+            position += INCREMENT;
+            robot.guider.setPosition(position);
+                 } else {
+            // Keep stepping down until we hit the min value.
+            position -= INCREMENT;
+            robot.guider.setPosition(position);
+            }
+       // robot.guider.setPosition(position);
+        //Picker
+        // Send telemetry message to signify robot running;
+        // telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+        telemetry.addData("left", "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+        telemetry.addData("shooter", shooter);
+        telemetry.addData("guider", guider);
+        telemetry.addData("Servo Position", "%5.2f", position);
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
